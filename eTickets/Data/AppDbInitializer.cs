2ae9cf72b3cc61cb6af0988cf1daadceaf282aa3
rozetadavchevska <rozetadavcevska@gmail.com>
2023-04-24 16:47:43 +0200

    Models and migration

---
+++ eTickets/Data/AppDbInitializer.cs
@@ -0,0 +1,129 @@
+﻿using eTickets.Data.Enums;
+using eTickets.Models;
+
+namespace eTickets.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+                context.Database.EnsureCreated();
+
+                if (!context.Cinemas.Any())
+                {
+                    context.Cinemas.AddRange(new List<Cinema>()
+                    {
+                        new Cinema()
+                        {
+                            Name = "Cinema 1",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-1.jpeg",
+                            Description = "This is the description of the first cinema"
+                        },
+                        new Cinema()
+                        {
+                            Name = "Cinema 2",
+                            Logo = "http://dotnethow.net/images/cinemas/cinema-2.jpeg",
+                            Description = "This is the description of the first cinema"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Actors.Any())
+                {
+                    context.Actors.AddRange(new List<Actor>()
+                    {
+                        new Actor()
+                        {
+                            ActorName = "Actor 1",
+                            Bio = "This is the Bio of the first actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-1.jpeg"
+
+                        },
+                        new Actor()
+                        {
+                            ActorName = "Actor 2",
+                            Bio = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/actors/actor-2.jpeg"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Producers.Any())
+                {
+                    context.Producers.AddRange(new List<Producer>()
+                    {
+                        new Producer()
+                        {
+                            ProducerName = "Producer 1",
+                            Bio = "This is the Bio of the first actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-1.jpeg"
+
+                        },
+                        new Producer()
+                        {
+                            ProducerName = "Producer 2",
+                            Bio = "This is the Bio of the second actor",
+                            ProfilePictureUrl = "http://dotnethow.net/images/producers/producer-2.jpeg"
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.Movies.Any())
+                {
+                    context.Movies.AddRange(new List<Movie>()
+                    {
+                        new Movie()
+                        {
+                            Name = "Life",
+                            Description = "This is the Life movie description",
+                            Price = 39.50,
+                            ImageUrl = "http://dotnethow.net/images/movies/movie-3.jpeg",
+                            StartDate = DateTime.Now.AddDays(-10),
+                            EndDate = DateTime.Now.AddDays(10),
+                            CinemaId = 2,
+                            ProducerId = 1,
+                            MovieCategory = MovieCategory.Documentary
+                        },
+                        new Movie()
+                        {
+                            Name = "The Shawshank Redemption",
+                            Description = "This is the Shawshank Redemption description",
+                            Price = 29.50,
+                            ImageUrl = "http://dotnethow.net/images/movies/movie-1.jpeg",
+                            StartDate = DateTime.Now,
+                            EndDate = DateTime.Now.AddDays(3),
+                            CinemaId = 1,
+                            ProducerId = 1,
+                            MovieCategory = MovieCategory.Action
+                        }
+                    });
+                    context.SaveChanges();
+                }
+
+                if (!context.ActorsMovies.Any())
+                {
+                    context.ActorsMovies.AddRange(new List<ActorMovie>()
+                    {
+                        new ActorMovie()
+                        {
+                            ActorId = 1,
+                            MovieId = 1
+                        },
+                        new ActorMovie()
+                        {
+                            ActorId = 2,
+                            MovieId = 1
+                        }
+                    });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
