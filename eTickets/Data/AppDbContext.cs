9f41fcd4fef736235811681e15113b271b471670
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-10-05 18:35:17 +0200

    added Order and OrderItem models

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -35,5 +35,8 @@ namespace eTickets.Data
         public DbSet<ActorMovie> ActorsMovies { get; set; }
         public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
     }
 }
