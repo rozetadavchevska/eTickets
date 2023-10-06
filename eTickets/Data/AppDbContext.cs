2eff004f6975fa4d44bfde6d845ec5f2c6e65179
Rozeta Davchevska <rozetadavcevska@gmail.com>
2023-10-06 19:14:36 +0200

    added ShoppingCartItem model

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -38,5 +38,6 @@ namespace eTickets.Data
 
         public DbSet<Order> Orders { get; set; }
         public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
